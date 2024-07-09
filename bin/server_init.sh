57640f73fe0543a628a166d87df5c69a1c8ea3b5
Duane Goodner <dmgoodner@gmail.com>
2024-07-08 19:36:49 -0600

    fix path to server_init.sh

--- bin/server_init.sh
+++ bin/server_init.sh
@@ -5,7 +5,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 DOT_ENV_FILE="$SCRIPT_DIR/../.env"
 # shellcheck disable=SC1090
 source "$DOT_ENV_FILE"
-SERVER_SCRIPT_LOCAL_PATH="$SCRIPT_DIR/server_init.sh"
+SERVER_SCRIPT_LOCAL_PATH="$SCRIPT_DIR/create_repo_dirs.sh"
 
 # Initialize an empty array for SUBVOL_LIST
 SUBVOL_LIST=()
@@ -20,17 +20,14 @@ done
 # Convert SUBVOL_LIST to a space-separated string
 SUBVOL_LIST_STR="${SUBVOL_LIST[*]}"
 
-
 # shellcheck disable=SC2087
 ssh -i "$SSH_KEYFILE" "$RESTIC_SERVER_USER@$RESTIC_SERVER" 'sudo -S bash -s' << EOF
-$(zenity --password)
+$(zenity --password --title="sudo on $RESTIC_SERVER" --text="Enter sudo password")
 export RESTIC_REPOS_DIR="$RESTIC_REPOS_DIR"
 export SUBVOL_LIST_STR="$SUBVOL_LIST_STR"
 export RESTIC_SERVER_USER="$RESTIC_SERVER_USER"
 $(cat "$SERVER_SCRIPT_LOCAL_PATH")
 EOF
-
-echo "${SUBVOL_LIST[@]}"
 
 for repo_name in "${SUBVOL_LIST[@]}"; do
   cur_repo=sftp:"$RESTIC_SERVER_USER"@"$RESTIC_SERVER":"$RESTIC_REPOS_DIR"/"$repo_name"
